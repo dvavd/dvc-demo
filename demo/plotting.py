df172d1830e5fd5e88750c27ae4d0d4b3a8c0cc7
DavidCWQ <caowenqi4812@163.com>
2024-10-04 10:54:03 +0200

    fix: plotting error of scatterplot

--- demo/plotting.py
+++ demo/plotting.py
@@ -47,12 +47,13 @@ bike_filtered = bike[(bike['prop_casual'] > 0) & (bike['prop_casual'] < 1)]
 for day_type in ['weekdays', 'weekends']:
     # Filter data based on 'day_type'
     day_type_prop = bike_filtered[bike_filtered['day_type'] == day_type][['prop_casual', 'temp']]
+    prop = day_type_prop
 
     # Apply LOWESS smoothing
-    ysmooth = lowess(day_type_prop['prop_casual'], day_type_prop['temp'] * 41, return_sorted=False)
+    ysmooth = lowess(day_type_prop['prop_casual'], day_type_prop['temp'], return_sorted=False)
 
     # Plot the smoothed line
-    sns.lineplot(day_type_prop['temp'] * 41, ysmooth, label=day_type)
+    sns.lineplot(day_type_prop['temp'].to_numpy(), ysmooth, label=day_type)
 # ==================================================================
 
 # Temperature Comparison Scatter Plot ==============================
@@ -60,7 +61,7 @@ for day_type in ['weekdays', 'weekends']:
     # Sample n data from dataset for plotting
     sampled_data = bike_filtered[bike_filtered['day_type'] == day_type].sample(n = 200)
 
-    xobs = (sampled_data ['temp'] * 41).to_numpy()
+    xobs = (sampled_data ['temp']).to_numpy()
     yobs = sampled_data ['prop_casual'].to_numpy()
 
     # Plot with sampled data points
